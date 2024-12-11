58aea0bb7593fbdb1a95fd9fc8a614267983173b
Yoni Kliger <yoni@vhive.ai>
2024-12-11 17:30:42 +0200

    implement filter

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,28 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {RecyclingSidebarComponent} from './filters/filters.component';
-import { GoogleMap } from '@angular/google-maps';
+import {GoogleMap} from '@angular/google-maps';
+import {ApiHandlerService} from "./services/api-handler.service";
+import {firstValueFrom} from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, RecyclingSidebarComponent, GoogleMap],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, RecyclingSidebarComponent, GoogleMap],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  options: google.maps.MapOptions = {
-    center: {lat: 40, lng: -20},
-    zoom: 4
-  };
+    options: google.maps.MapOptions = {
+        center: {lat: 40, lng: -20},
+        zoom: 4
+    };
+
+    protected recycleBins = [];
+
+    constructor(private apiHandler: ApiHandlerService) {
+    }
+
+    async onFiltersSubmit(selectedFilters: string[]) {
+        this.recycleBins = await firstValueFrom(this.apiHandler.getRecyclingData(selectedFilters));
+    }
 }
--- client/src/app/services/api-handler.service.ts
+++ client/src/app/services/api-handler.service.ts
@@ -3,12 +3,12 @@ import {HttpClient} from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
 export class ApiHandlerService {
-  private apiUrl = 'http://localhost:3000';
+    private apiUrl = 'http://localhost:3000';
 
-  constructor(private httpClient: HttpClient) {
-  }
+    constructor(private httpClient: HttpClient) {
+    }
 
-  getRecyclingData() {
-    return this.httpClient.get(`${this.apiUrl}/recycle-bins`);
-  }
+    getRecyclingData(selectedFilters: string[]) {
+        return this.httpClient.get<[]>(`${this.apiUrl}/recycle-bins?types[]=${selectedFilters.join(',')}`);
+    }
 }

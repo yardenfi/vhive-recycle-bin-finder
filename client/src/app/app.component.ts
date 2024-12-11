fb4ece9e79dc523850bd4481682d395742dace8c
yarde <yarden.fishler@gmail.com>
2024-12-11 17:19:26 +0200

    Added maps

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {RecyclingSidebarComponent} from './filters/filters.component';
+import { GoogleMap } from '@angular/google-maps';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RecyclingSidebarComponent],
+  imports: [RouterOutlet, RecyclingSidebarComponent, GoogleMap],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
+  options: google.maps.MapOptions = {
+    center: {lat: 40, lng: -20},
+    zoom: 4
+  };
 }
